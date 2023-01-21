daf805716094ad7f8bd1939a7ee396bdf1208fa3
20120262 <122881544+20120262@users.noreply.github.com>
2023-01-21 12:24:26 +0700

    Update app.js
    sdasd

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
+'use strict';
 
-app.get('/', (req, res) => res.send('<h1> Hello World! <h1>'))
+const express = require('express');
 
-app.listen(port)
-console.log('Listening on port ' + port)
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+    res.send('IF YOU CAN SEE THIS LINE IT MEAN THE CONTAINER IS RUNNING SUCCESSFUL');
+});
+
+app.get('/newpage', (req, res) => {
+    res.send("a very new page now");
+})
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
